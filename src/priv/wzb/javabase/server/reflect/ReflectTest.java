f1a11dc7f6835235d7e8ead6c36602578133ca2c
Satsuki <767218875@qq.com>
2019-09-10 20:04:47 +0800

    Test

---
+++ src/priv/wzb/javabase/server/reflect/ReflectTest.java
@@ -0,0 +1,36 @@
+package priv.wzb.javabase.server.reflect;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author Satsuki
+ * @time 2019/9/10 19:43
+ * @description:
+ * 反射:把java类中的各种结构(方法、属性、构造器、类名）映射成一个个的Java对象
+ * 1.获取class对象
+ * 三种方式:Class.forName("完整路径")
+ * 2.动态创建对象
+ * clz.getConstructor().newInstance();
+ */
+public class ReflectTest {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        //三种方式
+        //1.对象.getClass()
+        Class clz = new Iphone().getClass();
+        //2.类.class()
+        clz = Iphone.class;
+        //3.Class.forName("packageName.className")
+        clz = Class.forName("priv.wzb.javabase.server.reflect.Iphone");
+
+        //创建对象
+//        Iphone iphone = (Iphone) clz.newInstance();
+//        System.out.println(iphone);
+        Iphone iphone = (Iphone) clz.getConstructor().newInstance();
+
+        System.out.println(iphone);
+    }
+}
+class Iphone{
+    public Iphone() {
+    }
+}
---
+++ src/priv/wzb/javabase/server/reflect/XmlTest01.java
@@ -0,0 +1,9 @@
+package priv.wzb.javabase.server.reflect;
+
+/**
+ * @author Satsuki
+ * @time 2019/9/10 20:01
+ * @description:
+ */
+public class XmlTest01 {
+}
